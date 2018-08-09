28b421f7fca544d3a27db844da437e342e2dbfa2
MisakaTang <mikasatang@gmail.com>
2018-08-09 15:50:36 +0800

    增加sitemap功能

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path
 
 from blog import views
+from blog.sitemaps import PostSitemap
 
 app_name = 'blog'
 
+sitemaps = {
+    'posts': PostSitemap,
+}
 urlpatterns = [
     # 非Class-view 方式
     path('', views.post_list, name='post_list'),
@@ -11,5 +16,5 @@ urlpatterns = [
     # path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ]
-
