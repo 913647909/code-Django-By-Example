35edeff036a5e00edd558abf7040b5ccd1eb1a68
MisakaTang <mikasatang@gmail.com>
2018-08-13 10:38:18 +0800

    增加全文搜索功能插件

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models import Count
@@ -7,7 +8,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from taggit.models import Tag
 
-from blog.forms import EmailPostForm, CommentForm
+from blog.forms import EmailPostForm, CommentForm, SearchForm
 from blog.models import Post
 
 
@@ -79,3 +80,19 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            # results = Post.objects.annotate(search=SearchVector('title', 'body'), ).filter(search=query)
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.objects.annotate(similarity=TrigramSimilarity('title', query), ). \
+                filter(similarity__gt=0.3).order_by('-similarity')
+    return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
